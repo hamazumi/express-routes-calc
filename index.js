2594be9a024e5e77c0ded9cf520a65e12a8a8293
hamazumi <khamamoto12@gmail.com>
2021-05-27 16:14:55 -0700

    DONE

--- index.js
+++ index.js
@@ -6,33 +6,52 @@ app.get('/', (req, res) => {
     res.send('hello, hello')
 })
 
-//ADDITION
-app.get('/add/:x/:y', (req, res) => {
+// //ADDITION
+// app.get('/add/:x/:y', (req, res) => {
+//     const x = parseInt(req.params.x)
+//     const y = parseInt(req.params.y)
+//     res.send(`${x + y}`)
+// })
+
+// //SUBTRACTION
+// app.get('/subtract/:x/:y', (req, res) => {
+//     const x = parseInt(req.params.x)
+//     const y = parseInt(req.params.y)
+//     res.send(`${x - y}`)
+// })
+
+// //MULTIPLY
+// app.get('/multiply/:x/:y', (req, res) => {
+//     const x = parseInt(req.params.x)
+//     const y = parseInt(req.params.y)
+//     res.send(`${x * y}`)
+// })
+
+// //DIVIDE
+// app.get('/divide/:x/:y', (req, res) => {
+//     const x = parseInt(req.params.x)
+//     const y = parseInt(req.params.y)
+//     res.send(`${x / y}`)
+// })
+
+//COMBINED
+app.get(“/:z/:x/:y”, (req, res) => {
+    const z = req.params.z
     const x = parseInt(req.params.x)
     const y = parseInt(req.params.y)
-    res.send(`${x + y}`)
+    if (z == “add”) {
+        res.send(“<h1>” + ${x} + ${y} = ${x + y}. + “</h1>“)
+    } else if (z == “subtract”) {
+        res.send(“<h1>” + ${x} - ${y} = ${x - y}. + “</h1>“)
+    } else if (z == “multiply”) {
+        res.send(“<h1>” + ${x} x ${y} = ${x * y}. + “</h1>“)
+    } else if (z == “divide”) {
+        res.send(“<h1>” + ${x} / ${y} = ${x / y}. + “</h1>“)
+    } else {
+        res.send(“<h1>wut</h1>“)
+    }
 })
 
-//SUBTRACTION
-app.get('/subtract/:x/:y', (req, res) => {
-    const x = parseInt(req.params.x)
-    const y = parseInt(req.params.y)
-    res.send(`${x - y}`)
-})
-
-//MULTIPLY
-app.get('/multiply/:x/:y', (req, res) => {
-    const x = parseInt(req.params.x)
-    const y = parseInt(req.params.y)
-    res.send(`${x * y}`)
-})
-
-//DIVIDE
-app.get('/divide/:x/:y', (req, res) => {
-    const x = parseInt(req.params.x)
-    const y = parseInt(req.params.y)
-    res.send(`${x / y}`)
-})
 
 //Listen to PORT
 app.listen(PORT, () => {
